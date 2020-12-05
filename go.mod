eee395f539f212a270bd542205a3aa9fe3fc71fd
cfi <carlo@swiss.dev>
2020-12-05 08:58:14 +0100

    update for more performance

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/cfi2017/aoc-go
 
 go 1.15
+
+require golang.org/x/sys v0.0.0-20201204225414-ed752295db88
--- main.go
+++ main.go
@@ -1,68 +1,70 @@
 package main
 
 import (
-  "fmt"
-  "io/ioutil"
-  "os"
-  "strings"
-  "time"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+	"time"
+)
+
+var (
+	useAVX2 bool
+	useAVX  bool
+	useSSE4 bool
 )
 
 func main() {
-  input := GetInput(os.Args[1])
-  max, id := Compute(input)
-  fmt.Printf("max is %d, id is %d", max, id)
+	input := GetInput(os.Args[1])
+	max, id := Compute(input)
+	fmt.Printf("max is %d, id is %d", max, id)
 }
 
 func ComputeInput(input string) (max, id uint16) {
-  return Compute(GetInput(input))
+	return Compute(GetInput(input))
 }
 
 func GetInput(input string) []string {
-  bs, err := ioutil.ReadFile(input)
-  if err != nil {
-    panic(err)
-  }
-  return strings.Split(string(bs), "\n")
+	bs, err := ioutil.ReadFile(input)
+	if err != nil {
+		panic(err)
+	}
+	return strings.Split(string(bs), "\n")
 }
 
 func Compute(lines []string) (max, id uint16) {
-  var seats [1025]uint8
-  for _, line := range lines {
-    seat := Parse(line)
-    seats[seat] = 1
-  }
-  for seats[id] == 0 {
-    id++
-  }
-  for seats[id] == 1 {
-    id++
-  }
-  max = id + 1
-  for seats[max] == 1 {
-    max++
-  }
-  max--
-  return
+	var seats [1025]uint8
+	for _, line := range lines {
+		seat := Parse(line)
+		seats[seat] = 1
+	}
+	for seats[id] == 0 {
+		id++
+	}
+	for seats[id] == 1 {
+		id++
+	}
+	max = id + 1
+	for seats[max] == 1 {
+		max++
+	}
+	max--
+	return
 }
 
 func Parse(line string) (r uint16) {
-  for i := 0; i < 7; i++ {
-    r = r << 1
-    if line[i] == 'B' {
-      r++
-    }
-  }
-  for i := 7; i < 10; i++ {
-    r = r << 1
-    if line[i] == 'R' {
-      r++
-    }
-  }
-  return r
+	for i := 0; i < 7; i++ {
+		r = r << 1
+		r += 1 - uint16((line[i]&4)>>2)
+	}
+	for i := 7; i < 10; i++ {
+		r = r << 1
+		r += uint16((line[i] & 2) >> 1)
+	}
+	return r
 }
 
 func t(start time.Time, function string) {
-  r := time.Since(start)
-  fmt.Printf("%s took %v\n", function, r)
+	r := time.Since(start)
+	fmt.Printf("%s took %v\n", function, r)
 }
